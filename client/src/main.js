cf7f8b47b4e0a8a30ee14866fe938efaced305cb
Dan6erbond <moravrav@gmail.com>
2020-12-22 13:26:04 +0100

    Bootstrap new Vue project

--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,12 @@
+// import Buefy from "buefy";
 import { createApp } from "vue";
 import App from "./App.vue";
+import "./assets/scss/app.scss";
 import router from "./router";
 import store from "./store";
 
 createApp(App)
   .use(store)
   .use(router)
+  // .use(Buefy)
   .mount("#app");

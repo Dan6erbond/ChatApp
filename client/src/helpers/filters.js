58389f6a6205f1c7192a36226505efb84893713a
Dan6erbond <moravrav@gmail.com>
2020-12-24 12:21:30 +0100

    Create `date` and `time` filters

--- client/src/helpers/filters.js
+++ client/src/helpers/filters.js
@@ -8,3 +8,17 @@ Vue.filter("datetime", value => {
     .toLocaleTimeString()
     .slice(0, -3)}`;
 });
+
+Vue.filter("date", value => {
+  if (!value) return "";
+  if (typeof value.getMonth !== "function") return "";
+
+  return value.toLocaleDateString();
+});
+
+Vue.filter("time", value => {
+  if (!value) return "";
+  if (typeof value.getMonth !== "function") return "";
+
+  return value.toLocaleTimeString().slice(0, -3);
+});

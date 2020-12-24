4ddd34277fab7421f9bc1a9783bb386142fcdcee
Dan6erbond <moravrav@gmail.com>
2020-12-24 12:21:18 +0100

    Update main app view with nav and main container

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,34 @@
 <template>
   <div id="app">
-    <router-view />
+    <b-navbar shadow fixed-top class="main-nav" type="is-primary" centered>
+      <template slot="brand">
+        <router-link
+          :to="user ? '/chats' : '/'"
+          class="mt-2 mx-2 has-text-white"
+        >
+          <message-icon :size="48"></message-icon>
+        </router-link>
+      </template>
+    </b-navbar>
+    <main role="main">
+      <router-view />
+    </main>
   </div>
 </template>
+
+<script>
+import MessageIcon from "vue-material-design-icons/Message.vue";
+import { mapState } from "vuex";
+
+export default {
+  components: {
+    MessageIcon,
+  },
+  computed: {
+    ...mapState(["user"]),
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
@@ -13,16 +39,11 @@
   padding: 30px;
 }
 
-#nav {
-  padding: 30px;
+.main-nav {
+  z-index: 100 !important;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+main {
+  padding-top: 50px;
 }
 </style>

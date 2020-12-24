baad19a4a5719eb3727a6e519cc59dbe7c3ba9ff
Dan6erbond <moravrav@gmail.com>
2020-12-24 20:32:32 +0100

    Fetch users and subscribe to user event in frontend to display users with status

--- client/src/App.vue
+++ client/src/App.vue
@@ -10,6 +10,23 @@
         </router-link>
       </template>
     </b-navbar>
+    <section class="sidebar-layout">
+      <b-sidebar position="fixed" open type="is-light" fullheight scroll="clip">
+        <div class="p-1">
+          <b-menu>
+            <b-menu-list label="Users">
+              <b-menu-item
+                icon="account"
+                v-for="user in usersToDisplay"
+                :key="user.id"
+                :label="user.username + ' - ' + user.status"
+              >
+              </b-menu-item>
+            </b-menu-list>
+          </b-menu>
+        </div>
+      </b-sidebar>
+    </section>
     <main role="main">
       <router-view />
     </main>
@@ -19,13 +36,48 @@
 <script>
 import MessageIcon from "vue-material-design-icons/Message.vue";
 import { mapState } from "vuex";
+import GET_USERS from "@/graphql/GetUsers.gql";
+import SUBSCRIBE_TO_USERS from "@/graphql/SubscribeToUsers.gql";
 
 export default {
   components: {
     MessageIcon,
   },
+  data() {
+    return {
+      users: [],
+    };
+  },
   computed: {
     ...mapState(["user"]),
+    usersToDisplay() {
+      return this.users.filter((u) => {
+        if (this.user) {
+          return u.id !== this.user.id;
+        }
+        return true;
+      });
+    },
+  },
+  apollo: {
+    users: {
+      query: GET_USERS,
+      subscribeToMore: {
+        document: SUBSCRIBE_TO_USERS,
+        updateQuery(previousResult, { subscriptionData }) {
+          previousResult.users = previousResult.users.map((u) => {
+            if (u.id === subscriptionData.data.onUserOnlineStatus.user.id) {
+              return {
+                ...u,
+                status: subscriptionData.data.onUserOnlineStatus.status,
+              };
+            }
+            return u;
+          });
+          return previousResult;
+        },
+      },
+    },
   },
 };
 </script>
@@ -45,5 +97,13 @@ export default {
 
 main {
   padding-top: 50px;
+  padding-left: 250px;
+}
+
+.b-sidebar {
+  .sidebar-content {
+    padding-top: 80px;
+    padding-left: 10px;
+  }
 }
 </style>
